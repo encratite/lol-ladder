848d1df1a8fdf0548092908f472530ed638a6344
void <git.email.address@googlemail.com>
2011-12-31 18:04:38 +0100

    Changed the output to obtain a plain text table instead

--- analyse.rb
+++ analyse.rb
@@ -1,4 +1,5 @@
 require 'nil/serialise'
+require 'nil/console'
 
 require_relative 'PlayerResult'
 
@@ -23,6 +24,14 @@ class RatingStatistics
   end
 end
 
+def floatingPointString(input, percentage = true)
+  output = sprintf('%.1f', input)
+  if percentage
+    output += '%'
+  end
+  return output
+end
+
 def analyse
   players = Nil.deserialise('data/results')
   granularity = 50
@@ -36,14 +45,33 @@ def analyse
   end
   ratingStatistics = ratingStatistics.values.sort
   offset = 0
+  rows = [
+    [
+      'Elo',
+      'Games played',
+      'Win ratio',
+      'Win/loss difference',
+      'Percentile',
+    ]
+  ]
   ratingStatistics.each do |statistics|
     games = (statistics.wins + statistics.losses).to_f / statistics.count
     winRatio = statistics.wins.to_f / (statistics.wins + statistics.losses) * 100.0
     moreWinsThanLosses = (statistics.wins - statistics.losses).to_f / statistics.count
     percentile = offset.to_f / players.size * 100.0
-    printf("**#{statistics.range} Elo**: %.1f games played on average with a win ratio of %.1f%% and %.1f more wins than losses, percentile within listed population: %.1f%%\n\n", games, winRatio, moreWinsThanLosses, percentile)
+    adjustedPercentile = 75.0 + percentile / 4
+    #printf("#{statistics.range} Elo: %.1f games played on average with a win ratio of %.1f%% and %.1f more wins than losses, percentile within listed population: %.1f%%, percentile within total population: about %.1f%%\n", games, winRatio, moreWinsThanLosses, percentile, adjustedPercentile)
+    row = [
+      statistics.range.to_s,
+      floatingPointString(games, false),
+      floatingPointString(winRatio),
+      '+' + floatingPointString(moreWinsThanLosses, false),
+      floatingPointString(adjustedPercentile),
+    ]
+    rows << row
     offset += statistics.count
   end
+  Nil.printTable(rows)
 end
 
 analyse

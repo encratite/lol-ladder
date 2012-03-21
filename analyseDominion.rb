d2910ff7936e392a7719655fe9b81ba9e45b5f8f
void <git.email.address@googlemail.com>
2012-03-21 05:00:43 +0100

    Added a new script for Jabe's Dominion data and adjusted the estimated 1200 percentile adjustment in the original script accordingly

--- analyseDominion.rb
+++ analyseDominion.rb
@@ -1,4 +1,4 @@
-require 'nil/serialise'
+require 'nil/file'
 require 'nil/console'
 
 require_relative 'PlayerResult'
@@ -32,8 +32,7 @@ def floatingPointString(input, percentage = true)
   return output
 end
 
-def analyse
-  players = Nil.deserialise('data/results')
+def analyse(players)
   granularity = 50
   ratingStatistics = {}
   players.each do |player|
@@ -59,14 +58,12 @@ def analyse
     winRatio = statistics.wins.to_f / (statistics.wins + statistics.losses) * 100.0
     moreWinsThanLosses = (statistics.wins - statistics.losses).to_f / statistics.count
     percentile = offset.to_f / players.size * 100.0
-    adjustedPercentile = 50.0 + percentile / 2
-    #printf("#{statistics.range} Elo: %.1f games played on average with a win ratio of %.1f%% and %.1f more wins than losses, percentile within listed population: %.1f%%, percentile within total population: about %.1f%%\n", games, winRatio, moreWinsThanLosses, percentile, adjustedPercentile)
     row = [
       statistics.range.to_s,
       floatingPointString(games, false),
       floatingPointString(winRatio),
-      '+' + floatingPointString(moreWinsThanLosses, false),
-      floatingPointString(adjustedPercentile),
+      (moreWinsThanLosses >= 0 ? '+' : '') + floatingPointString(moreWinsThanLosses, false),
+      floatingPointString(percentile),
     ]
     rows << row
     offset += statistics.count
@@ -74,4 +71,30 @@ def analyse
   Nil.printTable(rows)
 end
 
-analyse
+def loadData(path)
+  lines = Nil.readLines(path)
+  players = []
+  pattern = /(\d+)(.+?) (?:NA|EUW|EUN) .*?(\d+).*?(\d+)W\/(\d+)L.*?(?:\d+):(?:\d+):(?:\d+):(?:\d+)/
+  lines.each do |line|
+    match = pattern.match(line)
+    if match == nil
+      next
+    end
+    rank = match[1].to_i
+    name = match[2].strip
+    rating = match[3].to_i
+    wins = match[4].to_i
+    losses = match[5].to_i
+    player = PlayerResult.new(rank, name, wins, losses, rating)
+    if rating < 400 || rating > 2700
+      puts line
+      puts player.inspect
+      exit
+    end
+    players << player
+  end
+  return players
+end
+
+players = loadData(ARGV[0])
+analyse(players)
